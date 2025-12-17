2a783432104bbdfaf3636d9dafe4d8bed24f5a2d
Ayush Sharma <ayu.sharma.303@gmail.com>
2025-12-18 00:33:41 +0530

    work update

---
+++ index.js
@@ -0,0 +1,4 @@
+const heading = [React.createElement("h1", {id:"h1", style:{color:"green"}}, "hi my name is ayush" ),
+                    React.createElement("h2", {id:"h2", style:{color:"red"}}, "hi my name is ayush" )]
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
