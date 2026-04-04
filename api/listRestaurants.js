ac1bd054992368fcd8e17ee452723a9c2b635bea
Ayush Sharma <ayu.sharma.303@gmail.com>
2026-04-04 16:52:49 +0530

    revised Mobile Responsive Commit 2

--- api/listRestaurants.js
+++ api/listRestaurants.js
@@ -1,6 +1,6 @@
 const { execFile } = require('child_process');
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
